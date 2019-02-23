d5f66ec5114f8932776377b255141be1f5f3c3e1
embedeepEric <herh@embedeep.com>
2019-02-23 16:24:05 +0800

    update

--- Runtime_Software/examples/testf3_lenet.sh
+++ Runtime_Software/examples/testf3_lenet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
-basepath=${basepath}"/../tpudemo"
+basepath=${basepath}"/../eepdemo"
 cd ${basepath}
-sudo ./eepdemo_arm --bin ../tpubin/freetpu_a3c8/lenet/eeptpu.bin --image ../images/lenet/7.jpg --label 7 --net lenet
+sudo ./eepdemo_arm --bin ../tpubin/freetpu_a3c8/lenet/eeptpu.bin --image ../images/lenet/00000_7.pgm --label 7 --net lenet
 cd - > /dev/null
