69e6f0d99a57e2ba81804180075ed5edb328f9e1
embedeepEric <herh@embedeep.com>
2019-03-01 16:31:57 +0800

    update freetpu bin files

--- Runtime_Software/examples/testf3_mobilenet_v1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-basepath=$(cd `dirname $0`; pwd)
-basepath=${basepath}"/../eepdemo"
-cd ${basepath}
-sudo ./eepdemo_arm --bin ../tpubin/freetpu_a3c8/mobilenet1/nobn/eeptpu.bin --image ../images/classify/ILSVRC2012_val_00000049.JPEG --label 717 --net mobilenet
-cd - > /dev/null
